79c36937ab19d712ab6bb0dd82206e01f232ca4e
Wangdi Tenzing <wangdi457@gmail.com>
2024-05-16 12:02:59 -0400

    We have the base of the page set up now Created Routes Ticket Page Ticket cards and did some basic styling using Tailwind Css Need to work on setting up the database now

--- app/{components}/TicketCard.jsx
+++ app/{components}/TicketCard.jsx
@@ -5,11 +5,26 @@ import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
-      <ProgressDisplay />
-      <StatusDisplay />
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4> Ticket Title</h4>
+      <hr className="h-px border-o bg-page mb-2" />
+      <p className="whitespace-pre-wrap">this is the ticket desc..</p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1"> 05/16/24 11:56 am</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
