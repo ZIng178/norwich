1f2e80399f808faa06a0855c644ea2f0b9b6d83b
Wangdi Tenzing <wangdi457@gmail.com>
2024-05-19 13:10:43 -0400

    Passing the progress props through the progress display component so that it renders the progress dynamically StatusDisplay component also displays the status acoording to its use case wrote a condition for the priority display so that it updates dynamically So far we can POST and GET data from the app need to make the app UPDATE and DELETE as well

--- app/{components}/PriorityDisplay.jsx
+++ app/{components}/PriorityDisplay.jsx
@@ -6,11 +6,11 @@ const PriorityDisplay = ({ priority }) => {
     <div className="=flex justify-start align-baseline">
       <FontAwesomeIcon
         icon={faFire}
-        className={`pr-1 ${priority > 1 ? "text-red-400" : "text-slate-400"}`}
+        className={`pr-1 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
       />
       <FontAwesomeIcon
         icon={faFire}
-        className={`pr-1 ${priority > 2 ? "text-red-400" : "text-slate-400"}`}
+        className={`pr-1 ${priority > 1 ? "text-red-400" : "text-slate-400"}`}
       />
       <FontAwesomeIcon
         icon={faFire}
--- app/{components}/TicketCard.jsx
+++ app/{components}/TicketCard.jsx
@@ -4,6 +4,21 @@ import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = ({ ticket }) => {
+  const formatTimeStamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString("en-US", options);
+
+    return formattedDate;
+  };
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
@@ -12,17 +27,17 @@ const TicketCard = ({ ticket }) => {
           <DeleteBlock />
         </div>
       </div>
-      <h4> Ticket Title</h4>
+      <h4> {ticket.title}</h4>
       <hr className="h-px border-o bg-page mb-2" />
-      <p className="whitespace-pre-wrap">this is the ticket desc..</p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1"> 05/16/24 11:56 am</p>
-          <ProgressDisplay />
+          <p className="text-xs my-1"> {formatTimeStamp(ticket.createdAt)}</p>
+          <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
-          <StatusDisplay />
+          <StatusDisplay status={ticket.status} />
         </div>
       </div>
     </div>
